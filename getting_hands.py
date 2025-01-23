c7bc88de58f0c0f68edb4f4c01d84c4c1750ef1d
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2025-01-23 12:05:08 +0100

    WIP

--- getting_hands.py
+++ getting_hands.py
@@ -54,12 +54,50 @@ def is_flush(cards):
     counters = {1:0,2:0,3:0,4:0}
     for i in cols:
         counters[i] += 1
-    print(counters)
     for i in counters:
         if counters[i] >= 5:
             return True
     return False
-
+def get_matching(cards):
+    cds = {}
+    tre = 0
+    pairs = 0
+    for ca in cards:
+        if ca < 100:
+            co = int(ca/10)
+            nu = str(ca/10).split(".")[1]
+        else:
+            co = int(ca/100)
+            nu = str(ca/100).split(".")[1]
+        
+        if nu == "1":
+                nu = 10 
+        cds[ca] = (co,int(nu))
+    cols = []
+    vals = []
+    for i in cds:
+        cols.append(cds[i][0])
+        vals.append(cds[i][1])
+    vals_dict = {}
+    for i in vals:
+        vals_dict[i]=0
+    for i in vals:
+        vals_dict[i] += 1
+    for i in vals_dict:
+        if vals_dict[i] == 2:
+            pairs += 1
+        if vals_dict[i] == 3:
+            tre += 1
+    print(pairs,tre)
+    if pairs >= 1 and tre >= 1:
+        return "fullhouse"
+    if tre == 1:
+        return "three"
+    if pairs == 1:
+        return "1pair"
+    if pairs == 2:
+        return "2pair"
+    return "highcard"
 def random_c():
     return ac.pop(random.randint(0,len(ac)-1))
 
@@ -85,10 +123,12 @@ def readable_c(ca):
     rc += f" {str(nu)}"
     return rc
 
+def check_hands(cards):
+    pass
 
 karten = []
 for i in range(0,7):
     karten.append(random_c())
 for i in karten:
     print(f"{i}:{readable_c(i)}")
-print(is_flush(karten))
+print(get_matching(karten))
